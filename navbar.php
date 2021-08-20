1009675f2a3b3456e2d402cd7523cb185d3c1b68
Aayan-07 <shahid08akhtar@gmail.com>
2021-08-20 20:03:38 +0530

    upload

---
+++ navbar.php
@@ -0,0 +1,17 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-blue bg-blue">
+      <a class="navbar-brand" href="index.php" style="color : #C0392B; font-weight: bolder;">THE BANK OF PEOPLE</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #C0392B; font-weight: bolder;">HOME</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #C0392B; font-weight: bolder;">HISTORY</a>
+              </li>
+          </div>
+       </nav>
